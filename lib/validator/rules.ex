d4e8e64044a3888593338d73a15d8d1e8b2f8d31
atanda rasheed <atandarash@gmail.com>
2025-01-22 04:21:11 +0100

    refactor: improve rules implementation

--- lib/validator/rules.ex
+++ lib/validator/rules.ex
@@ -3,14 +3,13 @@ defmodule Request.Validator.Rulex do
   alias EmailChecker.Check.MX
   alias Request.Validator.Utils
 
-  import Gettext.Macros
+  import Gettext.Macros, only: [gettext_with_backend: 3, gettext_with_backend: 2]
 
   require Decimal
 
   @type rule :: %{
           required(:name) => atom(),
-          required(:validator) => (... -> boolean()),
-          required(:message) => binary(),
+          required(:validator) => (... -> :ok | {:error, String.t()}),
           optional(:implicit?) => boolean()
         }
 
@@ -20,43 +19,50 @@ defmodule Request.Validator.Rulex do
              Request.Validator.Gettext
            )
 
+  defmacrop gettext(msgid, opts) do
+    quote do
+      gettext_with_backend(@backend, unquote(msgid), unquote(opts))
+    end
+  end
+
   @doc """
   ## Examples
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = required()
-  iex> fun.("")
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.([])
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.("hello world")
-  true
-  iex> fun.([1, 2])
-  true
-  iex> fun.(%{"hello" => "world"})
-  true
+  iex> fun.("name", "")
+  {:error, "The name field is required."}
+  iex> fun.("gender", nil)
+  {:error, "The gender field is required."}
+  iex> fun.("coordinates", [])
+  {:error, "The coordinates field is required."}
+  iex> fun.("metadata", %{})
+  {:error, "The metadata field is required."}
+  iex> fun.("message", "hello world")
+  :ok
+  iex> fun.("products", [1, 2])
+  :ok
+  iex> fun.("metadata", %{"hello" => "world"})
+  :ok
   """
   @spec required() :: rule()
   def required do
     # TODO: check for empty `Plug.Upload`.
-    validator_fn = fn value ->
+    validator_fn = fn attr, value ->
+      message = gettext("The %{attribute} field is required.", attribute: attr)
+
       cond do
-        is_nil(value) -> false
-        is_binary(value) and String.trim(value) == "" -> false
-        Enumerable.impl_for(value) != nil and Enum.empty?(value) -> false
-        true -> true
+        is_nil(value) -> {:error, message}
+        is_binary(value) and String.trim(value) == "" -> {:error, message}
+        Enumerable.impl_for(value) != nil and Enum.empty?(value) -> {:error, message}
+        true -> :ok
       end
     end
 
     %{
       name: :required,
       implicit?: true,
-      validator: validator_fn,
-      message: gettext_with_backend(@backend, "The :attribute field is required.")
+      validator: validator_fn
     }
   end
 
@@ -65,32 +71,72 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = required_if(true)
-  iex> fun.("")
-  false
-  iex> fun.(nil)
-  false
+  iex> fun.("description", "")
+  {:error, "The description field is required."}
+  iex> fun.("age", nil)
+  {:error, "The age field is required."}
   iex> %{validator: fun} = required_if(fn -> false end)
-  iex> fun.("")
-  true
-  iex> fun.(nil)
-  true
+  iex> fun.("key", "")
+  :ok
+  iex> fun.("state", nil)
+  :ok
+  iex> %{validator: fun} = required_if(["passenger", "kid"])
+  iex> data = %{"passenger" => "kid"}
+  iex> fun.("parent", nil, data)
+  {:error, "The parent field is required when passenger is kid."}
   """
-  @spec required_if((... -> boolean()) | boolean()) :: rule()
+  @spec required_if((... -> boolean()) | boolean() | [term()]) :: rule()
   def required_if(condition) when is_function(condition) or is_boolean(condition) do
     %{validator: required} = required()
 
     validator_fn = fn
-      false, _, _ -> true
-      true, value, _fun -> required.(value)
-      cond_fn, value, fun when is_function(cond_fn) -> fun.(cond_fn.(), value, fun)
+      false, _, _, _ -> :ok
+      true, attr, value, _fun -> required.(attr, value)
+      cond_fn, attr, value, fun when is_function(cond_fn) -> fun.(cond_fn.(), attr, value, fun)
     end
 
     %{
       name: :required_if,
       implicit?: true,
-      validator: &validator_fn.(condition, &1, validator_fn),
-      message:
-        gettext_with_backend(@backend, "The :attribute field is required when :other is :value.")
+      validator: &validator_fn.(condition, &1, &2, validator_fn)
+    }
+  end
+
+  def required_if([other, value]) do
+    required_if(other, value, &Kernel.==/2)
+  end
+
+  # TODO: allow other operators and tailor error appropriately.
+  defp required_if(other, value, op) do
+    %{validator: required} = required()
+
+    check_fn = fn attr, value, op, data ->
+      attr_path = Utils.convert_to_path(attr)
+      other = get_in(data, attr_path)
+
+      op.(other, value)
+    end
+
+    validator_fn = fn
+      false, _, _, _, _ ->
+        :ok
+
+      true, attr, value, other, cond ->
+        message =
+          gettext(
+            "The %{attribute} field is required when %{other} is %{value}.",
+            attribute: attr,
+            other: other,
+            value: cond
+          )
+
+        check(required.(attr, value) == :ok, message)
+    end
+
+    %{
+      name: :required_if,
+      implicit?: true,
+      validator: &(check_fn.(other, value, op, &3) |> validator_fn.(&1, &2, other, value))
     }
   end
 
@@ -99,25 +145,26 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = string()
-  iex> fun.("")
-  true
-  iex> fun.(1)
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("content", "")
+  :ok
+  iex> fun.("content", 1)
+  {:error, "The content field must be a string."}
+  iex> fun.("content", nil)
+  {:error, "The content field must be a string."}
+  iex> fun.("content", %{})
+  {:error, "The content field must be a string."}
+  iex> fun.("content", [])
+  {:error, "The content field must be a string."}
   """
   @spec string() :: rule()
   def string do
-    %{
-      name: :string,
-      validator: &is_binary/1,
-      message:
-        gettext_with_backend(@backend, "The :attribute field is required when :other is :value.")
-    }
+    validator_fn = fn attr, value ->
+      message = gettext("The %{attribute} field must be a string.", attribute: attr)
+
+      check(is_binary(value), message)
+    end
+
+    %{name: :string, validator: &validator_fn.(&1, &2)}
   end
 
   @doc """
@@ -125,24 +172,29 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = alpha()
-  iex> fun.("abcde")
-  true
-  iex> fun.(1)
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("uid", "abcde")
+  :ok
+  iex> fun.("uid", 1)
+  {:error, "The uid field must only contain letters."}
+  iex> fun.("uid", nil)
+  {:error, "The uid field must only contain letters."}
+  iex> fun.("uid", %{})
+  {:error, "The uid field must only contain letters."}
+  iex> fun.("uid", [])
+  {:error, "The uid field must only contain letters."}
   """
   @spec alpha() :: rule()
   def alpha do
-    %{
-      name: :alpha,
-      validator: &(is_binary(&1) and String.match?(&1, ~r/^[a-zA-Z]+$/)),
-      message: gettext_with_backend(@backend, "The :attribute may only contain letters.")
-    }
+    validator_fn = fn attr, value ->
+      message = gettext("The %{attribute} field must only contain letters.", attribute: attr)
+
+      value
+      |> is_binary()
+      |> Kernel.and(String.match?(value, ~r/^[a-zA-Z]+$/))
+      |> check(message)
+    end
+
+    %{name: :alpha, validator: &validator_fn.(&1, &2)}
   end
 
   @doc """
@@ -150,33 +202,34 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = alpha_num()
-  iex> fun.("1ab2de3")
-  true
-  iex> fun.(1)
-  true
-  iex> fun.("abc_de2")
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("ref", "1ab2de3")
+  :ok
+  iex> fun.("ref", 1)
+  {:error, "The ref field must only contain letters and numbers."}
+  iex> fun.("ref", "abc_de2")
+  {:error, "The ref field must only contain letters and numbers."}
+  iex> fun.("ref", nil)
+  {:error, "The ref field must only contain letters and numbers."}
+  iex> fun.("ref", %{})
+  {:error, "The ref field must only contain letters and numbers."}
+  iex> fun.("ref", [])
+  {:error, "The ref field must only contain letters and numbers."}
   """
   @spec alpha_num() :: rule()
   def alpha_num do
-    validator_fn = fn
-      value when is_number(value) -> true
-      value when is_binary(value) -> String.match?(value, ~r/^[a-zA-Z0-9]+$/)
-      _value -> false
+    validator_fn = fn attr, value ->
+      message =
+        gettext("The %{attribute} field must only contain letters and numbers.",
+          attribute: attr
+        )
+
+      value
+      |> is_binary()
+      |> Kernel.and(String.match?(value, ~r/^[a-zA-Z0-9]+$/))
+      |> check(message)
     end
 
-    %{
-      name: :alpha_num,
-      validator: &validator_fn.(&1),
-      message:
-        gettext_with_backend(@backend, "The :attribute may only contain letters and numbers.")
-    }
+    %{name: :alpha_num, validator: &validator_fn.(&1, &2)}
   end
 
   @doc """
@@ -184,36 +237,35 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = alpha_dash()
-  iex> fun.("abcde2")
-  true
-  iex> fun.("ab_d-2")
-  true
-  iex> fun.(1)
-  true
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("username", "abcde2")
+  :ok
+  iex> fun.("username", "ab_d-2")
+  :ok
+  iex> fun.("username", 1)
+  {:error, "The username field must only contain letters, numbers, dashes, and underscores."}
+  iex> fun.("username", nil)
+  {:error, "The username field must only contain letters, numbers, dashes, and underscores."}
+  iex> fun.("username", %{})
+  {:error, "The username field must only contain letters, numbers, dashes, and underscores."}
+  iex> fun.("username", [])
+  {:error, "The username field must only contain letters, numbers, dashes, and underscores."}
   """
   @spec alpha_dash() :: rule()
   def alpha_dash do
-    validator_fn = fn
-      value when is_number(value) -> true
-      value when is_binary(value) -> String.match?(value, ~r/^[a-zA-Z0-9-_]+$/)
-      _value -> false
+    validator_fn = fn attr, value ->
+      message =
+        gettext(
+          "The %{attribute} field must only contain letters, numbers, dashes, and underscores.",
+          attribute: attr
+        )
+
+      value
+      |> is_binary()
+      |> Kernel.and(String.match?(value, ~r/^[a-zA-Z0-9-_]+$/))
+      |> check(message)
     end
 
-    %{
-      name: :alpha_dash,
-      validator: &validator_fn.(&1),
-      message:
-        gettext_with_backend(
-          @backend,
-          "The :attribute may only contain letters, numbers, dashes and underscores."
-        )
-    }
+    %{name: :alpha_dash, validator: &validator_fn.(&1, &2)}
   end
 
   @doc """
@@ -221,26 +273,28 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = integer()
-  iex> fun.(1)
-  true
-  iex> fun.(2.0)
-  false
-  iex> fun.("abcde2")
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("age", 1)
+  :ok
+  iex> fun.("age", 2.0)
+  {:error, "The age field must be an integer."}
+  iex> fun.("age", "abcde2")
+  {:error, "The age field must be an integer."}
+  iex> fun.("age", nil)
+  {:error, "The age field must be an integer."}
+  iex> fun.("age", %{})
+  {:error, "The age field must be an integer."}
+  iex> fun.("age", [])
+  {:error, "The age field must be an integer."}
   """
   @spec integer() :: rule()
   def integer do
-    %{
-      name: :integer,
-      validator: &is_integer/1,
-      message: gettext_with_backend(@backend, "The :attribute must be an integer.")
-    }
+    validator_fn = fn attr, value ->
+      message = gettext("The %{attribute} field must be an integer.", attribute: attr)
+
+      check(is_integer(value), message)
+    end
+
+    %{name: :integer, validator: &validator_fn.(&1, &2)}
   end
 
   @doc """
@@ -248,28 +302,30 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = decimal()
-  iex> fun.(2.0)
-  true
-  iex> fun.(Decimal.new("9.999"))
-  true
-  iex> fun.(1)
-  false
-  iex> fun.("abcde2")
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("amount", 2.0)
+  :ok
+  iex> fun.("amount", Decimal.new("9.999"))
+  :ok
+  iex> fun.("amount", 1)
+  {:error, "The amount field must be a decimal."}
+  iex> fun.("amount", "abcde2")
+  {:error, "The amount field must be a decimal."}
+  iex> fun.("amount", nil)
+  {:error, "The amount field must be a decimal."}
+  iex> fun.("amount", %{})
+  {:error, "The amount field must be a decimal."}
+  iex> fun.("amount", [])
+  {:error, "The amount field must be a decimal."}
   """
   @spec decimal() :: rule()
   def decimal do
-    %{
-      name: :decimal,
-      validator: &(is_float(&1) or Decimal.is_decimal(&1)),
-      message: gettext_with_backend(@backend, "The :attribute must be an decimal.")
-    }
+    validator_fn = fn attr, value ->
+      message = gettext("The %{attribute} field must be a decimal.", attribute: attr)
+
+      check(is_float(value) or Decimal.is_decimal(value), message)
+    end
+
+    %{name: :decimal, validator: &validator_fn.(&1, &2)}
   end
 
   @doc """
@@ -277,26 +333,28 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = numeric()
-  iex> fun.(2.0)
-  true
-  iex> fun.(1)
-  true
-  iex> fun.("abcde2")
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("width", 2.0)
+  :ok
+  iex> fun.("width", 1)
+  :ok
+  iex> fun.("width", "abcde2")
+  {:error, "The width field must be a number."}
+  iex> fun.("width", nil)
+  {:error, "The width field must be a number."}
+  iex> fun.("width", %{})
+  {:error, "The width field must be a number."}
+  iex> fun.("width", [])
+  {:error, "The width field must be a number."}
   """
   @spec numeric() :: rule()
   def numeric do
-    %{
-      name: :numeric,
-      validator: &is_number/1,
-      message: gettext_with_backend(@backend, "The :attribute must be a number.")
-    }
+    validator_fn = fn attr, value ->
+      message = gettext("The %{attribute} field must be a number.", attribute: attr)
+
+      check(is_number(value), message)
+    end
+
+    %{name: :numeric, validator: &validator_fn.(&1, &2)}
   end
 
   @doc """
@@ -304,22 +362,22 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = email()
-  iex> fun.("test@gmail.com")
-  true
+  iex> fun.("email", "test@gmail.com")
+  :ok
   iex> %{validator: fun} = email([:format])
-  iex> fun.("a@b.com")
-  true
+  iex> fun.("email", "a@b.com")
+  :ok
   iex> %{validator: fun} = email(["mx"])
-  iex> fun.("a@b.com")
-  false
-  iex> fun.(2.0)
-  false
-  iex> fun.(nil)
-  false
-  iex> fun.(%{})
-  false
-  iex> fun.([])
-  false
+  iex> fun.("email", "a@b.com")
+  {:error, "The email field must be a valid email address."}
+  iex> fun.("email", 2.0)
+  {:error, "The email field must be a valid email address."}
+  iex> fun.("email", nil)
+  {:error, "The email field must be a valid email address."}
+  iex> fun.("email", %{})
+  {:error, "The email field must be a valid email address."}
+  iex> fun.("email", [])
+  {:error, "The email field must be a valid email address."}
   """
   @email_checks %{format: Format, mx: MX}
   @spec email([:format | :mx | String.t()]) :: rule()
@@ -330,11 +388,13 @@ defmodule Request.Validator.Rulex do
         validations -> validations |> Enum.map(&Utils.to_atom/1) |> Enum.map(&@email_checks[&1])
       end
 
-    %{
-      name: :email,
-      validator: &(is_binary(&1) and EmailChecker.valid?(&1, validations)),
-      message: gettext_with_backend(@backend, "The :attribute must be a valid email address.")
-    }
+    validator_fn = fn validations, attr, value ->
+      message = gettext("The %{attribute} field must be a valid email address.", attribute: attr)
+
+      check(is_binary(value) and EmailChecker.valid?(value, validations), message)
+    end
+
+    %{name: :email, validator: &validator_fn.(validations, &1, &2)}
   end
 
   @doc """
@@ -348,34 +408,35 @@ defmodule Request.Validator.Rulex do
   ...> }
   iex> %{validator: fun} = confirmed()
   iex> fun.("password", 12345678, data)
-  true
+  :ok
   iex> fun.("list.0.a", 1, data)
-  true
+  :ok
   iex> fun.("password", "yikes!", data)
-  false
+  {:error, "The password field confirmation does not match."}
   iex> fun.("list.0.a", 10, data)
-  false
+  {:error, "The list.0.a field confirmation does not match."}
   """
   @spec confirmed(nil | String.t()) :: rule()
-  def confirmed(attr \\ nil) do
-    validator_fn = fn field, value, data ->
-      attr =
-        case attr do
-          nil -> field <> "_confirmation"
-          attr -> attr
+  def confirmed(confirmation \\ nil) do
+    validator_fn = fn confirmation, attr, value, data ->
+      confirmation =
+        case confirmation do
+          nil -> attr <> "_confirmation"
+          confirmation -> confirmation
         end
 
       # INFO: maybe not convert to path and have the data already flatten?
-      attr_path = Utils.convert_to_path(attr)
+      confirmation_path = Utils.convert_to_path(confirmation)
 
-      get_in(data, attr_path) == value
+      message = gettext("The %{attribute} field confirmation does not match.", attribute: attr)
+
+      data
+      |> get_in(confirmation_path)
+      |> Kernel.==(value)
+      |> check(message)
     end
 
-    %{
-      name: :confirmed,
-      validator: &validator_fn.(&1, &2, &3),
-      message: gettext_with_backend(@backend, "The :attribute confirmation does not match.")
-    }
+    %{name: :confirmed, validator: &validator_fn.(confirmation, &1, &2, &3)}
   end
 
   @doc """
@@ -383,21 +444,32 @@ defmodule Request.Validator.Rulex do
 
   iex> import Request.Validator.Rulex
   iex> %{validator: fun} = allowed(["male", "female"])
-  iex> fun.("male")
-  true
-  iex> fun.("female")
-  true
-  iex> fun.("child")
-  false
-  iex> fun.("goat")
-  false
+  iex> fun.("gender", "male")
+  :ok
+  iex> fun.("gender", "female")
+  :ok
+  iex> fun.("gender", "child")
+  {:error, "The selected gender is invalid."}
+  iex> fun.("gender", "goat")
+  {:error, "The selected gender is invalid."}
   """
   @spec allowed([term()]) :: rule()
-  def allowed(values) do
-    %{
-      name: :allowed,
-      validator: &Enum.member?(values, &1),
-      message: gettext_with_backend(@backend, "The selected :attribute is invalid.")
-    }
+  def allowed(options) do
+    validator_fn = fn options, attr, value ->
+      message = gettext("The selected %{attribute} is invalid.", attribute: attr)
+
+      options
+      |> Enum.member?(value)
+      |> check(message)
+    end
+
+    %{name: :allowed, validator: &validator_fn.(options, &1, &2)}
+  end
+
+  defp check(cond, message) do
+    case cond do
+      true -> :ok
+      false -> {:error, message}
+    end
   end
 end
